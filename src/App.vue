a8a85e9479aa0b90d50d35f04d84f6a67cf6db4f
Wang, Xiaofeng <xwang@webmd.net>
2018-12-14 14:10:40 -0500

    add a initResize() for display:none issue

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,47 @@
   <div id="app">
     <!--<vue-leaflet-plugin></vue-leaflet-plugin>-->
     <!--<leafletmap id="myfirstmap"></leafletmap>-->
-<div class="first-container">
-    <Lmap id="myfirstmap"></Lmap>
+    <div class="first-container">
+      <Lmap id="myfirstmap"></Lmap>
 
-</div>
+    </div>
     <br>
+    <button @click="handleClick()">show/hide map</button>
 
-    <div class="second-container">
-    <Lmap id="mysecondmap"></Lmap>
+
+    <div v-show='showMap' class="second-container">
+      <Lmap id="mysecondmap" ref="secondMap"></Lmap>
     </div>
   </div>
 </template>
 
 <script>
-  import leafletmap from './lib/map.vue'
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'app',
+    data() {
+      return {
+        showMap: false,
+      }
+    },
+    components: {},
+    methods: {
+      handleClick() {
+        this.showMap = !this.showMap;
+        this.$refs.secondMap.initResize();
+      }
     }
-  },
-  components:{
-    leafletmap
   }
-}
 </script>
 
 <style lang="scss">
-  .first-container{//map container is necessary deafult map will width:100% height:100%
-  /*margin:0 auto;*/
-  width:500px;
-  height: 500px;
-}
-  .second-container{
-    width:300px;
-    height: 300px;
+  .first-container { //map container is necessary deafult map will width:100% height:100%
+    /*margin:0 auto;*/
+    width: 200px;
+    height: 200px;
+  }
+
+  .second-container {
+    width: 200px;
+    height: 200px;
   }
 </style>
--- src/lib/map.vue
+++ src/lib/map.vue
@@ -7,38 +7,48 @@
 <script>
   import L from 'leaflet'
   import 'leaflet/dist/leaflet.css'
+  import Vue from 'vue'
 
-    export default {
-        name: "Lmap",
-      data(){
-          return {
-            options:{
-              zoom: 13,
-              center: [40.7128,-74.0060],
-              url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-              attribution: 'vue-leaflet',
-              draggable: true,
-            }
-          }
-      },props :{
-          id:{
-            type:String,
-            required:true,
-            default:'myfirstmap'
-          }
-      },
+  export default {
+    name: "Lmap",
+    data() {
+      return {
+        map: {},
+        options: {
+          zoom: 13,
+          center: [40.7128, -74.0060],
+          url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+          attribution: 'vue-leaflet',
+          draggable: true,
+        }
+      }
+    }, props: {
+      id: {
+        type: String,
+        required: true,
+        default: 'myfirstmap'
+      }
+    },
 
-      mounted(){
-          console.log(this.id);
-          let map=L.map(this.id,this.options);
-          L.tileLayer(this.options.url).addTo(map);
+    mounted() {
+      this.map = L.map(this.id, this.options);
+      L.tileLayer(this.options.url).addTo(this.map);
+    },
+    methods: {
+      initResize() {//this is for display:none scenario
+        if (this.map) {
+          Vue.nextTick(() => {
+            this.map.invalidateSize();
+          });
+        }
       }
     }
+  }
 </script>
 
 <style scoped lang="scss">
-.default-leaflet-map-container{
-  width:100%;
-  height:100%;
-}
+  .default-leaflet-map-container {
+    width: 100%;
+    height: 100%;
+  }
 </style>
